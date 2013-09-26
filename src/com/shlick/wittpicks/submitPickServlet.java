dd42e327d56426dc22fb898e196173a4ec36b625
Mike Marchlik <mikeymarchlik@gmail.com>
2013-09-26 16:57:47 -0400

    updated so all data from screen goes into datastore

--- src/com/shlick/wittpicks/submitPickServlet.java
+++ src/com/shlick/wittpicks/submitPickServlet.java
@@ -19,6 +19,7 @@ public class submitPickServlet extends HttpServlet {
 
 	private static final long serialVersionUID = 1L;
 	public   String pick_output_string;
+	public static final int CURRENT_WEEK = 4;
 
 @Override
 public void doPost(HttpServletRequest req, HttpServletResponse resp)
@@ -27,11 +28,19 @@ public void doPost(HttpServletRequest req, HttpServletResponse resp)
 	
 	String email_address = req.getParameter("picker_email");
 	pick_output_string = email_address + ", ";
-	//verifyEmail();
-
+	//TODO verifyEmail() - the email entered, is it listed in a file?  ie. grep for filename in file if exists true else false
+	
+	
+	//Setup a Datastore.  It seems synonymous with database...
+	Key userPicksKey = KeyFactory.createKey("UserEmail", email_address);
+	Entity my_picks = new Entity("picks", userPicksKey);
+	//Since we only have an email as a key, need current week to filter picks
+	my_picks.setProperty("week_number", CURRENT_WEEK);
+	
 	int i = 1;
 	while(true)
 	{
+		//Go through all of the games and store it in the Datastore
 		String current_game = "game" + i;
 		String content = req.getParameter(current_game);
 		if (content == null) {
@@ -39,21 +48,16 @@ public void doPost(HttpServletRequest req, HttpServletResponse resp)
 		}
 		else
 		{
-			pick_output_string += content + ", ";
+			my_picks.setProperty( current_game, content);
 			i++;
 		}
 	}
 	
-    Key userPicksKey = KeyFactory.createKey("UserEmail", email_address);
-
-    Entity picks_string = new Entity("userPicks", userPicksKey);
-    picks_string.setProperty("selections", pick_output_string);
-
+    //Now store off the data
     DatastoreService datastore = DatastoreServiceFactory.getDatastoreService();
-    datastore.put(picks_string);
+    datastore.put(my_picks);
 
 
-    
     resp.sendRedirect("/pickResults.jsp?userEmail=" + email_address);
 }
 
